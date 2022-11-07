ce14c9739b7082ac0731c83465a52d7598a84bba
unknown <muhammad khairunnas nurdin 1 F>
2022-11-07 22:16:08 +0700

    muhammad khairunnas n kelas 1 F

--- lib/b_inputoutput.dart
+++ lib/b_inputoutput.dart
@@ -9,4 +9,14 @@
 
 // hint: gunakan stdin.readLineSync() untuk membaca input dari user
 
-void main() {}
+import 'dart:io';
+
+void main() {
+  // stdout.write('masukan nama anda : ');
+  String nama = (stdin.readLineSync()) ?? 'anonymus';
+
+  // stdout.write('masukan umur :');
+  int umur = int.tryParse(stdin.readLineSync()!) ?? 0;
+
+  print('Halo, saya $nama, umur saya $umur tahun');
+}
